3016510f3233a9d9d8a9626017b9abe283535ff5
John Vincent <yehohanan7@gmail.com>
2025-02-25 19:48:59 +0000

    fix locale

--- next.config.js
+++ next.config.js
@@ -16,6 +16,10 @@ const nextConfig = {
     // https://nextjs.org/docs/api-reference/next.config.js/ignoring-typescript-errors
     ignoreBuildErrors: true,
   },
+  i18n: {
+    locales: ['en-US', 'fr', 'de'],
+    defaultLocale: 'en-US',
+  }
 }
 
 module.exports = nextConfig
